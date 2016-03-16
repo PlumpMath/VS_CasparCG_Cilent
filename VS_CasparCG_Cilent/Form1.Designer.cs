223d9c6da729f26882cbbf4c75af28385370d06f
Karl-Axel Zander <kazander@kth.se>
2016-03-16 15:09:57 +0100

    la till telnet-kod för att skicka till en casparCG server, men funkar inte än (crashar)

--- VS_CasparCG_Cilent/Form1.Designer.cs
+++ VS_CasparCG_Cilent/Form1.Designer.cs
@@ -29,38 +29,43 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.kommando = new System.Windows.Forms.TextBox();
+            this.ipadress = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
+            this.serverstatus = new System.Windows.Forms.Label();
+            this.port = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.servermessage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 231);
+            this.button1.Location = new System.Drawing.Point(96, 276);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(149, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Skicka Telnet-kommando";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
             // 
-            // textBox1
+            // kommando
             // 
-            this.textBox1.Location = new System.Drawing.Point(189, 205);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.kommando.Location = new System.Drawing.Point(117, 248);
+            this.kommando.Name = "kommando";
+            this.kommando.Size = new System.Drawing.Size(100, 20);
+            this.kommando.TabIndex = 1;
             // 
-            // textBox2
+            // ipadress
             // 
-            this.textBox2.Location = new System.Drawing.Point(189, 98);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
+            this.ipadress.Location = new System.Drawing.Point(189, 98);
+            this.ipadress.Name = "ipadress";
+            this.ipadress.Size = new System.Drawing.Size(100, 20);
+            this.ipadress.TabIndex = 2;
+            this.ipadress.Text = "127.0.0.1";
             // 
             // label1
             // 
@@ -74,7 +79,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(211, 189);
+            this.label2.Location = new System.Drawing.Point(129, 227);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 13);
             this.label2.TabIndex = 4;
@@ -93,34 +98,64 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(201, 124);
+            this.button2.Location = new System.Drawing.Point(199, 169);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 6;
             this.button2.Text = "Connect";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
             // 
-            // label4
+            // serverstatus
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(48, 101);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(94, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = " Not disconnected";
+            this.serverstatus.AutoSize = true;
+            this.serverstatus.Location = new System.Drawing.Point(39, 123);
+            this.serverstatus.Name = "serverstatus";
+            this.serverstatus.Size = new System.Drawing.Size(94, 13);
+            this.serverstatus.TabIndex = 7;
+            this.serverstatus.Text = " Not disconnected";
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(190, 141);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(100, 20);
+            this.port.TabIndex = 8;
+            this.port.Text = "5250";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(225, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Port";
+            // 
+            // servermessage
+            // 
+            this.servermessage.AutoSize = true;
+            this.servermessage.Location = new System.Drawing.Point(51, 186);
+            this.servermessage.Name = "servermessage";
+            this.servermessage.Size = new System.Drawing.Size(0, 13);
+            this.servermessage.TabIndex = 10;
+            this.servermessage.Click += new System.EventHandler(this.servermessage_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 300);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(361, 339);
+            this.Controls.Add(this.servermessage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.serverstatus);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ipadress);
+            this.Controls.Add(this.kommando);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "VS CasparCG Client";
@@ -133,13 +168,16 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox kommando;
+        private System.Windows.Forms.TextBox ipadress;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label serverstatus;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label servermessage;
     }
 }
 
--- VS_CasparCG_Cilent/Form1.cs
+++ VS_CasparCG_Cilent/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace VS_CasparCG_Cilent
 {
     public partial class Form1 : Form
     {
+        Socket s;
         public Form1()
         {
             InitializeComponent();
@@ -38,6 +42,33 @@ namespace VS_CasparCG_Cilent
         }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            string ip = ipadress.Text;
+            int ports = Int32.Parse(port.Text);
+
+            IPAddress[] IPs = Dns.GetHostAddresses(ip);
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect(IPs[0], ports);
+            serverstatus.Text = "Connected to server\n" + ip + "\n" + ports;
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+             byte[] command = Encoding.ASCII.GetBytes(kommando.Text);
+             s.Send(command);
+            kommando.Clear();
+             byte[] buffer = new byte[50];
+             s.Receive(buffer);
+             servermessage.Text = (Encoding.ASCII.GetString(buffer));
+        }
+
+        private void servermessage_Click(object sender, EventArgs e)
         {
 
         }
