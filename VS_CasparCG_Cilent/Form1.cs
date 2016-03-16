195378007fc9ea6d9b7d8e385c7bc155c1281fec
Karl-Axel Zander <kazander@kth.se>
2016-03-16 13:59:05 +0100

    karl-axel button 4

--- VS_CasparCG_Cilent/Form1.cs
+++ VS_CasparCG_Cilent/Form1.cs
@@ -16,5 +16,10 @@ namespace VS_CasparCG_Cilent
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
